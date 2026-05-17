d2ec2a7de59ad1c2b1a9e88bf1509237d62463a5
Bolotinhas <glodsonborges@gmail.com>
2026-05-17 12:31:59 -0300

    feat: delete update playback dto and implement create playback dto

--- backend/src/playback/dto/create-playback.dto.ts
+++ backend/src/playback/dto/create-playback.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePlaybackDto {}
+import {IsInt,IsOptional, IsString, Length} from 'class-validator';
+
+export class CreatePlaybackDto {
+    @IsString()
+    @Length(3,20)
+    login: string
+
+    @IsOptional()
+    @IsInt()
+    musicaid?: number;
+
+    @IsOptional()
+    @IsInt()
+    episodeid?: number;
+}
--- backend/src/playback/dto/update-playback.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePlaybackDto } from './create-playback.dto';
-
-export class UpdatePlaybackDto extends PartialType(CreatePlaybackDto) {}
